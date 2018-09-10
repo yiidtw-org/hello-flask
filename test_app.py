27b69de4c2735f0767eaa026788d897a7f44ccd2
yiidtw <yiidtw@gmail.com>
2018-09-10 19:48:31 +0800

    modify unittest

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import *
 
 class TestHello(unittest.TestCase):
     def test_hello(self):
-       self.assertEqual("Hello Roger", hello()) 
+       self.assertEqual("Hello Warren", hello()) 
 
 if __name__ == '__main__':
     unittest.main()
