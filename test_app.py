8689de84ce921d2c8744f468e45fefe009a51dbe
yiidtw <38613604+yiidtw@users.noreply.github.com>
2018-09-10 17:05:59 +0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import *
 
 class TestHello(unittest.TestCase):
     def test_hello(self):
-       self.assertEqual("Hello DevOpsDay", hello()) 
+       self.assertEqual("Hello Monday", hello()) 
 
 if __name__ == '__main__':
     unittest.main()
