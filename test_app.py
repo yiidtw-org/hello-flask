c58e2338d08089b9045251d2e3ffe9f3e8f522c0
yiidtw <yiidtw@gmail.com>
2018-09-10 16:33:01 +0800

    Hello DevOpsDay

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import *
 
 class TestHello(unittest.TestCase):
     def test_hello(self):
-       self.assertEqual("Hello Web", hello()) 
+       self.assertEqual("Hello DevOpsDay", hello()) 
 
 if __name__ == '__main__':
     unittest.main()
