c948c0764c15a60db84318a612fd10d7783b3f27
yiidtw <yiidtw@gmail.com>
2018-09-10 15:44:08 +0800

    add simple integration test

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    print 'INFO Incoming Hello from User-Agent: ' + request.headers['User-Agent']
     return "Hello World"
+
+@app.route("/integration-test")
+def integration_test():
+    print 'INFO Incoming Hello from User-Agent: ' + request.headers['User-Agent']
+    return "Got You"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
